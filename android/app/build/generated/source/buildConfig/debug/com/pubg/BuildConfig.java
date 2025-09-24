3612315f51355cae8e50828d6bb551aa8375061e
martinezgerald958-alt <martinezgerald958@gmail.com>
2025-09-24 18:02:37 +0800

    Update BuildConfig.java
    Yes

--- android/app/build/generated/source/buildConfig/debug/com/pubg/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/pubg/BuildConfig.java
@@ -2,7 +2,7 @@
  * Automatically generated file. DO NOT MODIFY
  */
 package com.pubg;
-
+*/
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.pubg";
